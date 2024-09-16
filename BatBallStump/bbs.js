bce972aa27f26f3761f7f9b834c491427edca37d
Imthiyaz-Shaik <20pp5a0275@gmail.com>
2024-09-16 16:39:04 +0530

    reduced code using functions and return statement

--- BatBallStump/bbs.js
+++ BatBallStump/bbs.js
@@ -1,18 +1,41 @@
 
-
-function ComputerGeneratedChoice()
-{
+function getcomputerChoice(){
     let randomNumber=Math.random()*3;
     console.log(randomNumber);
 
-    if(randomNumber >0 && randomNumber <=1)
+    if(randomNumber>0 && randomNumber<1)
+        {
+          return'Bat';
+        }
+        else if( randomNumber<2)
+        {
+           return'Ball';
+        }
+        else
+        {
+           return'Stump';
+        }
+}
 
-    {return'Bat';}
-
-    else if(randomNumber >1 && randomNumber <=2)
-
-        { return'Ball'; }
-
-    else
-     { return'Stump'; }
+function getResultMsg(userChoice,computerChoice)
+{
+if(userChoice===computerChoice)
+{
+   return 'it is a tie';
+}
+else if(computerChoice==='Stump' )
+{
+return 'computer has won';
+}
+else if( userChoice==='Stump')
+{
+   return 'user has won';
+}
+else if(computerChoice==='Bat'&& userChoice==='Ball')
+{
+   return 'compter has won';
+}
+else{
+   return 'user has won';
+}
 }
